f9831d527ca072ae85af90556b530c5d3d3fe4b8
Bryan Jensen <bryanawjensen@gmail.com>
2014-04-08 22:22:58 -0400

    Forgot to backup the header

---
+++ networkClassPrimitive.h
@@ -0,0 +1,43 @@
+#ifndef _NNCLASS
+#define _NNCLASS
+
+#include <iostream>
+#include <random>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+class NeuralNetwork {
+private:
+	long nLayers, nNodes;
+	float** net;
+	bool created;
+	int epochLimit;
+
+	default_random_engine generator;
+
+	void init();
+	void deleteLayers();
+	void createLayers();
+	void fillNet();
+public:
+	NeuralNetwork();
+	NeuralNetwork(const NeuralNetwork&);
+
+	void setNumNodes(int);
+	void setNumLayers(int);
+	void setEpochLimit(int);
+
+	void mutate();
+	void train(float, float);
+	void train(float*, int, float*, int);
+
+	float run(float);
+
+	NeuralNetwork& operator=(const NeuralNetwork&);
+
+	friend ostream& operator<<(ostream&, const NeuralNetwork&);
+};
+
+#endif
